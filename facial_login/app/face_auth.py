75bf3a966166e8c82d1573f9370d58fabf258ea2
JiwanSKhabra <jiwankhabra0@gmail.com>
2025-06-17 23:31:37 -0400

    Created Connection with database

--- facial_login/app/face_auth.py
+++ facial_login/app/face_auth.py
@@ -1,17 +1,44 @@
+import os
+import psycopg2
+from deepface import DeepFace
+
 class FaceAuth:
 
-    #
-    def __init__(self, db_path = 'pass'):
-        self.db_path = db_path
+    def __init__(self, dbname='health_monitoring', user='postgres', host='127.0.0.1', port='5432'):
+        self.db_config = {
+            'dbname': dbname,
+            'user': user,
+            'password': os.getenv('PG_PASSWORD'),
+            'host': host,
+            'port': port
+        }
 
-
-    # load images, compare using deepface or custom
     def verify_face(self, img1, img2):
-        pass
+        output = DeepFace.verify(img1, img2)
+        return output
 
-    #extract embedding and store in file/DB
     def save_face(self, user_id, image):
-        pass
+        # Step 1: Extract embedding
+        embedding = DeepFace.represent(img_path=image, model_name="Facenet")[0]['embedding']
+
+        # Step 2: Format for PostgreSQL
+        vector_str = '[' + ','.join(map(str, embedding)) + ']'
+
+        # Step 3: Connect using config from __init__s
+        conn = psycopg2.connect(**self.db_config)
+        cur = conn.cursor()
+
+        # Step 4: Insert into DB
+        cur.execute(
+            "INSERT INTO face_embeddings (name, embedding) VALUES (%s, %s);",
+            (user_id, vector_str)
+        )
+
+        conn.commit()
+        cur.close()
+        conn.close()
+        print(f"✅ Saved face embedding for {user_id}")
+
 
     # load save embedding
     def load_face(self, user_id):
@@ -24,5 +51,4 @@ class FaceAuth:
 
 face_auth = FaceAuth()
 
-output = face_auth.verify_face('blah', 'blah')
-print(output)
+face_auth.save_face("user001", "data/img1.jpg")
